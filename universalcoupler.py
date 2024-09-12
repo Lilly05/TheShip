69acd77a1170f9eac4ecc7d327cd8bd37dbc993a
lilly kate <71640994+Lilly05@users.noreply.github.com>
2024-09-12 10:07:36 +0200

    Update universalcoupler.py

--- universalcoupler.py
+++ universalcoupler.py
@@ -4,13 +4,11 @@ import json
 
 app = Flask(__name__)
 
-# S3 Konfiguration (hardcodiert)
 S3_HOST = 'http://192.168.100.17:2016'
 S3_BUCKET = 'theship-permastore'
 S3_ACCESS_KEY = 'theship'
 S3_SECRET_KEY = 'theship1234'
 
-# S3 Client
 s3_client = boto3.client('s3', 
                          endpoint_url=S3_HOST,
                          aws_access_key_id=S3_ACCESS_KEY,
@@ -35,8 +33,6 @@ def get_messages_from_s3(source, destination):
         return []
 
 
-
-# API für Empfangen von Nachrichten
 @app.route('/<station>/receive', methods=['POST'])
 def download(station):
     try:
@@ -45,11 +41,10 @@ def download(station):
         destination = data['destination']
         message = get_messages_from_s3(source, destination)
         save_message_to_s3(destination, message)
-        return jsonify({"kind": "success"}), 200
+        return jsonify({"kind": "success"}), 200, {'Content-Type': 'text/css; charset=utf-8'}
     except Exception as e:
         return jsonify({"kind": "error", "message": str(e)}), 500
 
-# API für Senden von Nachrichten
 @app.route('/<station>/send', methods=['POST'])
 def upload(station):
     try:
@@ -58,7 +53,7 @@ def upload(station):
         destination = data['destination']
         message = data['data']
         save_message_to_s3(source, destination, message)
-        return jsonify({"kind": "success"}), 200
+        return jsonify({"kind": "success"}), 200, {'Content-Type': 'text/css; charset=utf-8'}
     except Exception as e:
         return jsonify({"kind": "error", "message": str(e)}), 500
 
