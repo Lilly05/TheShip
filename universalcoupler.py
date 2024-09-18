68ed8541972cf24e4e0f6dc5b48979f595e136da
lilly kate <71640994+Lilly05@users.noreply.github.com>
2024-09-18 09:34:02 +0200

    Update universalcoupler.py

--- universalcoupler.py
+++ universalcoupler.py
@@ -1,59 +1,51 @@
 from flask import Flask, request, jsonify
 import boto3
 import json
+import requests
+import base64
 
 app = Flask(__name__)
-
-S3_HOST = 'http://192.168.100.17:2016'
-S3_BUCKET = 'theship-permastore'
-S3_ACCESS_KEY = 'theship'
-S3_SECRET_KEY = 'theship1234'
-
-s3_client = boto3.client('s3', 
-                         endpoint_url=S3_HOST,
-                         aws_access_key_id=S3_ACCESS_KEY,
-                         aws_secret_access_key=S3_SECRET_KEY)
-
-def save_message_to_s3(source, destination, message):
-    key = f'{source}/to_{destination}/messages.json'
-    print(key)
-    s3_client.put_object(
-        Bucket=S3_BUCKET,
-        Key=key,
-        Body=json.dumps(message)
-    )
-
-def get_messages_from_s3(source, destination):
-    key = f'{source}/to_{destination}/messages.json'
-    try:
-        response = s3_client.get_object(Bucket=S3_BUCKET, Key=key)
-        data = response['Body'].read().decode('utf-8')
-        return json.loads(data)
-    except s3_client.exceptions.NoSuchKey:
-        return []
-
 
 @app.route('/<station>/receive', methods=['POST'])
 def download(station):
     try:
-        data = request.json
-        source = data['source']
-        destination = data['destination']
-        message = get_messages_from_s3(source, destination)
-        save_message_to_s3(destination, message)
-        return jsonify({"kind": "success"}), 200, {'Content-Type': 'application/json'}
+        response = requests.post('http://192.168.100.17:2029/receive')
+        data = response.json()
+    
+        received_message = data['received_messages'][0]
+        dest = received_message['dest']
+        msg = received_message['msg']
+
+        base64_bytes = base64.b64decode(msg)
+        decoded_message = base64_bytes.decode('utf-8')
+
+        decoded_json = json.loads(decoded_message)
+
+        message = decoded_json['message']
+
+        message_json = json.loads(message)
+        data_array = [
+            message_json['source'],       
+            message_json['destination'],  
+            message_json['data'],        
+            message_json['ts']         
+        ]
+
+        return jsonify({"kind": "success", "messages": [{"destination": dest, "data": data_array}]}), 200
     except Exception as e:
         return jsonify({"kind": "error", "message": str(e)}), 500
 
 @app.route('/<station>/send', methods=['POST'])
 def upload(station):
     try:
-        data = request.json
-        source = data['source']
-        destination = data['destination']
-        message = data['data']
-        save_message_to_s3(source, destination, message)
-        return jsonify({"kind": "success"}), 200, {'Content-Type': 'application/json'}
+        data = request.get_json()
+        
+        source = data.get('source')
+        data_array = data.get('data')
+
+        print(f"Empfangen von {source}: {data_array}")
+
+        return jsonify({"kind": "success"}), 200
     except Exception as e:
         return jsonify({"kind": "error", "message": str(e)}), 500
 
