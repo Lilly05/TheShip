eac8bbad1fc35e4df47adc6b063b6cbe02adef47
lilly kate <71640994+Lilly05@users.noreply.github.com>
2024-09-19 08:46:22 +0200

    Update universalcoupler.py

--- universalcoupler.py
+++ universalcoupler.py
@@ -11,29 +11,28 @@ def download(station):
     try:
         response = requests.post('http://192.168.100.17:2029/receive')
         data = response.json()
-    
+
         received_message = data['received_messages'][0]
         dest = received_message['dest']
         msg = received_message['msg']
+        print(msg)
 
-        base64_bytes = base64.b64decode(msg)
-        decoded_message = base64_bytes.decode('utf-8')
+        binary_data = base64.b64decode(msg)
 
-        decoded_json = json.loads(decoded_message)
+        binary_array = list(binary_data)
 
-        message = decoded_json['message']
+        return jsonify({
+            "kind": "success",
+            "messages": [
+                {
+                    "destination": dest,
+                    "data": binary_array
+                }
+            ]
+        }), 200
 
-        message_json = json.loads(message)
-        data_array = [
-            message_json['source'],       
-            message_json['destination'],  
-            message_json['data'],        
-            message_json['ts']         
-        ]
-
-        return jsonify({"kind": "success", "messages": [{"destination": dest, "data": data_array}]}), 200
     except Exception as e:
-        return jsonify({"kind": "error", "message": "Fehler"}), 500
+        return jsonify({"kind": "error", "message": str(e)}), 500
 
 @app.route('/<station>/send', methods=['POST'])
 def upload(station):
@@ -47,7 +46,7 @@ def upload(station):
 
         return jsonify({"kind": "success"}), 200
     except Exception as e:
-        return jsonify({"kind": "error", "message": "Fehler"}), 500
+        return jsonify({"kind": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2023)
