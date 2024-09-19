4c2dc1cb06722c21725c4716e8eafa14773fff4c
lilly kate <71640994+Lilly05@users.noreply.github.com>
2024-09-19 09:32:58 +0200

    Update universalcoupler.py

--- universalcoupler.py
+++ universalcoupler.py
@@ -38,19 +38,24 @@ def download(station):
 @app.route('/<station>/send', methods=['POST'])
 def upload(station):
     try:
-
         data = request.get_json(force=True)
-
         print(data)
 
         source = data.get('source')
         data_array = data.get('data')
+        binary_data = bytes(data_array)
 
         print(f"Empfangen von {source}: {data_array}")
+
+        base64_encoded_data = base64.b64encode(binary_data).decode('utf-8')
+        print(base64_encoded_data)
+
+        requests.post('http://192.168.100.17:2030/put_message', json={"sending_station": source, "base64data": base64_encoded_data})
 
         return jsonify({"kind": "success"}), 200
     except Exception as e:
         return jsonify({"kind": "error", "message": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2023)
